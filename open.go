e7e4c6942e069708bff570ab300cce0a389cbc27
Максим Мигунов <migunovmaksim44@gmail.com>
2025-03-20 15:52:11 +0300

    add open file to test

---
+++ open.go
@@ -0,0 +1,11 @@
+package testModule
+
+import "fmt"
+
+func printFromTestModule() {
+	fmt.Println("print From Test Module!")
+}
+
+func PrintFromTestModule() {
+	fmt.Println("Print From Test Module!")
+}
