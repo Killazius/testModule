4f95bd1aa898ea277849406b536b971eb1ce9799
Максим Мигунов <migunovmaksim44@gmail.com>
2025-03-20 15:45:52 +0300

    my first module

---
+++ internal/secret/secret/secret.go
@@ -0,0 +1,11 @@
+package secret
+
+import "fmt"
+
+func HelloFromSecret() {
+	fmt.Println("Hello from secret")
+}
+
+func helloFromSecret() {
+	fmt.Println("hello from secret! its a secret")
+}
---
+++ pkg/lib/lib.go
@@ -0,0 +1,11 @@
+package lib
+
+import "fmt"
+
+func HelloFromLib() {
+	fmt.Println("Hello from lib")
+}
+
+func helloFromLib() {
+	fmt.Println("hello from lib pkg secret func")
+}
